77f6dfa055ddf9176d98fba6510b6758a3bbacd8
Alex Menjivar <oalex.menjivar@gmail.com>
2019-10-30 10:56:28 -0600

    PrivateRoute code atribution

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
 
+// From Tyler McGinnis post: https://tylermcginnis.com/react-router-protected-routes-authentication/
+
 class PrivateRoute extends Component { 
   render() {
     const { component: Component, authedUser, ...rest } = this.props;
